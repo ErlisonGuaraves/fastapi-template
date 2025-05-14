7a213e6541875807566bb945b6ba19b3f6fe0535
erlison.santos <erlison.santos@guaraves.com.br>
2025-05-14 09:00:00 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Tamplate base para futuros projetos 🤓
+
+- dependencias para o projeto estão nessa referencia:
+https://fastapidozero.dunossauro.com/4.0/01/
+
+
+## Commit
+```
+    git rm --cached -r .coverage
+    git rm --cached -r fast_zero/__pycache__/
+    git rm --cached -r tests/__pycache__/
+
+```
---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+# por nomenclatura, todos os testes começam por 'test_' seguido do modulo que irá ser testado
+
+from http import HTTPStatus
+from fastapi.testclient import TestClient
+from fast_zero.app import app
+
+
+def test_root_retorna_ola_mundo():
+    """
+    Esse teste tem 3 etapas (AAA)
+        - A: Arrange - Arranjo
+        - A: ACT     - Executa a coisa (o SUT "System Under Test")
+        - A: Assert  - Garanta que A é A
+    """
+    # Arrange
+    client = TestClient(app)
+
+    # ACT
+    response = client.get('/')
+
+    # Assert
+    assert response.status_code == HTTPStatus.OK  # statuscode: 200
+    assert response.json() == {'Message': 'Hello world!'}
