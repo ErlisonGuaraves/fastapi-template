371b534c5a38313439c79178f4f04df42d4d943c
erlison.santos <erlison.santos@guaraves.com.br>
2025-05-21 13:15:53 -0300

    aula sobre criacao de schemas e como realizar testes para requisicoes posts e update

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,11 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from fast_zero.shemas import Message
+from fast_zero.shemas import Message, UserSchema, UserPublic, UserDb
 from http import HTTPStatus
 
 app = FastAPI()
 
+database = []
+
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return Message(message="Hello world!")
+    return Message(message='Hello world!')
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_users(user: UserSchema):
+    user_with_id = UserDb(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK)
+def read_users():
+    return {'users': database}
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDb(**user.model_dump(), id=user_id)
+
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User or id not found!'
+        )
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,23 +1,69 @@
 # por nomenclatura, todos os testes começam por 'test_' seguido do modulo que irá ser testado
 
 from http import HTTPStatus
-from fastapi.testclient import TestClient
-from fast_zero.app import app
 
 
-def test_root_retorna_ola_mundo():
+def test_root_retorna_ola_mundo(client):
     """
     Esse teste tem 3 etapas (AAA)
         - A: Arrange - Arranjo
         - A: ACT     - Executa a coisa (o SUT "System Under Test")
         - A: Assert  - Garanta que A é A
     """
-    # Arrange
-    client = TestClient(app)
-
     # ACT
     response = client.get('/')
 
     # Assert
     assert response.status_code == HTTPStatus.OK  # statuscode: 200
     assert response.json() == {'message': 'Hello world!'}
+
+
+def test_create_user(client):
+    """
+    Teste: Criação de usuário via POST /users/
+
+    Observações:
+        - O campo 'id' não é enviado, pois é gerado automaticamente.
+        - O schema de resposta é baseado em UserPublic (sem a senha).
+    """
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Erlison',
+            'password': 'secret',
+            'email': 'erlison@example.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'Erlison',
+        'email': 'erlison@example.com',
+    }
+
+
+def test_read_users(client):
+    """
+    Testa a leitura da lista de usuários cadastrados.
+
+    Este teste realiza uma requisição GET ao endpoint `/users/` e verifica se:
+    - O status de resposta é HTTP 200 (OK).
+    - O corpo da resposta contém a lista esperada de usuários com seus respectivos campos:
+        - id
+        - username
+        - email
+    """
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'Erlison',
+                'email': 'erlison@example.com',
+            }
+        ]
+    }
